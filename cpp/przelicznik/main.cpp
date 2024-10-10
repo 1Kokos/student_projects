09713d36371537e7139c335d185228c05fe23f72
1Kokos <mateusz.matlak@e-zstih.edu.pl>
2024-10-10 14:28:08 +0000

    studentmachine send

---
+++ cpp/przelicznik/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+float metr, kilometr;
+cout<<"Wprowadz liczbe kilometrow";
+cin>>kilometr;
+metr=kilometr * 1000;
+cout<<kilometr<<"kilometry="<<metr<<"metry"<<endl;
+	return 0;
+}
