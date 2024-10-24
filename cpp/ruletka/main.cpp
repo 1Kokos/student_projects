50531cd9914e26e4b27211bbc18ab9b743b0688d
1Kokos <mateusz.matlak@e-zstih.edu.pl>
2024-10-24 14:01:32 +0000

    wyslanie pracy

--- cpp/ruletka/main.cpp
+++ cpp/ruletka/main.cpp
@@ -8,14 +8,14 @@ int main(){
 
 int wybranaLiczba;
 
-cout<<"wybierz liczbe od 1 do 10:";
+cout<<"wybierz liczbe od 1 do 6:";
 cin>>wybranaLiczba;
 
-if(wybranaLiczba<1 || wybranaLiczba>10){
+if(wybranaLiczba<1 || wybranaLiczba>6){
 cout<<"blad: wybrano liczbe spoza zakresu" << endl;
 return 1;
 }
-int wylosowanaLiczba = rand() % 10 + 1;
+int wylosowanaLiczba = rand() % 6 + 1;
 
 cout<<"Program wylosował liczbe:" <<wylosowanaLiczba << endl;
 
